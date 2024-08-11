cd7891d25b4d8a9be013a405c737b60b244a88f6
logta <takenobu15@gmail.com>
2024-08-11 21:40:13 +0900

    add: storybookの設定を入れた

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../app/components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+ 
+        viteConfigPath: 'vite-sb.config.ts',
+ 
+      }},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
