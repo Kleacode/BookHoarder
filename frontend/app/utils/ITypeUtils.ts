954dbe0342b5d556d02be09c84463b88e8be60c4
logta <takenobu15@gmail.com>
2024-08-12 20:12:58 +0900

    fix: isObject関数にテストを加えた

--- frontend/app/utils/ITypeUtils.ts
+++ frontend/app/utils/ITypeUtils.ts
@@ -1,6 +1,12 @@
+import _ from "lodash"
+
 export type Obj = { [x: string]: unknown };
 
 export const isObject = (value: unknown): value is object => {
 	const type = typeof value;
-	return value !== null && (type === "object" || type === "function");
+	return value !== null && (type === "object" || type === "function") && !isArray(value);
+};
+
+export const isArray =  (value: unknown): value is Array<unknown> => {
+	return _.isArray(value)
 };
