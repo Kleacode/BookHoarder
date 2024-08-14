3ccd8a9de50eee7b934d7fa2490dea9e84544830
logta <takenobu15@gmail.com>
2024-08-14 20:22:21 +0900

    add: prettierとstylelintを追加

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	printWidth: 100,
+};
+
+export default config;
