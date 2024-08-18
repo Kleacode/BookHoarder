0fbd8b7abaad870e760ce91d97f55f9c10e963fe
Kleacode <100291013+Kleacode@users.noreply.github.com>
2024-08-19 00:53:10 +0900

    add: oapi-codegenによる自動生成

--- backend/generate.go
+++
@@ -1,3 +0,0 @@
-package backend
-
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target ogen --clean ../openapi/openapi.yaml
---
+++ backend/tools/tools.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+)
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml ../../openapi/gen/openapi.yaml
