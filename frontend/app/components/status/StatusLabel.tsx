2a37e9fcf4c8d3248376e125d7824a1ac7d2c9b1
Kleacode <100291013+Kleacode@users.noreply.github.com>
2024-08-31 19:39:10 +0900

    feat: StatusLabelコンポーネント作成

---
+++ frontend/app/components/status/StatusLabel.tsx
@@ -0,0 +1,14 @@
+import { StatusType } from "@/libs/schemas/schemaHealper"
+import { ConvertStatusTypeToLabel } from "@/utils/ConvertStatusType"
+
+export interface StatusLabelProps {
+    status: StatusType
+};
+
+export const StatusLabel = ({ status }: StatusLabelProps) => {
+    return (
+        <div>
+            {ConvertStatusTypeToLabel(status)}
+        </div>
+    )
+}
---
+++ frontend/app/utils/ConverStatusType.test.ts
@@ -0,0 +1,13 @@
+import { assert, describe, expect, it } from "vitest";
+import { ConvertStatusTypeToLabel } from "./ConvertStatusType";
+
+describe("isObject", () => {
+    it("ok", () => {
+        expect(ConvertStatusTypeToLabel("todo")).toBe("積読");
+        expect(ConvertStatusTypeToLabel("wip")).toBe("読書中");
+        expect(ConvertStatusTypeToLabel("done")).toBe("読了");
+    })
+    it("ng", () => {
+        assert.throws(() => ConvertStatusTypeToLabel("aaaaaa" as any), "invalid status type");
+    })
+});
