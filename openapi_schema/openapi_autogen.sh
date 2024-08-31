66df7ed70a0aa838a764fade230ee64f801a08c1
Kleacode <100291013+Kleacode@users.noreply.github.com>
2024-08-31 23:49:22 +0900

    feat: openAPIの自動生成スクリプトにfrontendの自動生成を追加

--- openapi_schema/openapi_autogen.sh
+++ openapi_schema/openapi_autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 BACKEND_CONTAINER_NAME=bookhoarder-back
+FRONTEND_CONTAINER_NAME=bookhoarder-front
 
 echo 1. Bundle openapi with redoc
 
@@ -9,5 +10,9 @@ ${SCRIPT_DIR}/redoc_bundle.sh
 echo 2. Generate backend files with oapi-gen
 
 docker exec -w /app/tools ${BACKEND_CONTAINER_NAME} go generate
+
+echo 3. Generate frontend files with openapi-typescript
+
+docker exec -w /app ${FRONTEND_CONTAINER_NAME} npm run generate:api
 
 echo Finish!
